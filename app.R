d8b0edc1ee3efdfd65bdbcf545eacec9a9874b05
benmbrew <benmbrew@gmail.com>
2018-02-05 21:27:40 -0500

    changed color and added in new text

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(shinythemes)
 
 source('global.R')
 
-ui <- dashboardPage(skin = 'blue',
+ui <- dashboardPage(skin = 'purple',
                     
                     
                     dashboardHeader(
@@ -44,19 +44,23 @@ ui <- dashboardPage(skin = 'blue',
                       ),
                       tabItems(
                         tabItem(tabName = 'welcome',
-                                jumbotron("Welcome!", "The Ontario Youth Compass tracks the wellbeing of youth across the province using data from a variety trusted sources. This web app allows for easy exploration, visualization, and access to data about youth in Ontario.",
-                                          button = FALSE
+                                jumbotron("Ontario Youth Compass", "The Ontario Youth Compass tracks the wellbeing of youth across the province using data from a variety trusted sources. 
+                                           This web app allows for easy exploration, visualization, 
+                                           and access to data from the last four Canadian Censuses (2001, 2006, 2011, 2016), as well as various 
+                                           other national and province-wide surveys. This web app is meant to accompany 'The Ontario Youth Compass: 
+                                           A Databook on Youth Wellbeing report published by YouthREX in 2018'.",
+                                           button = FALSE
                                           # buttonLabel = "Explore!"
                                 ),
                                 fluidRow(
                                   column(6, panel_div(class_type = "primary", panel_title = "App Directions",
-                                                      content = "How to use the app<br>One day soon<br>There will be instructions here.")),
+                                                      content = "There will soon be instructions here.")),
                                   column(6, panel_div("success", "App Maintenance",
                                                       "Email us: <a href='mailto:xing@databrew.cc?Subject=Stepping%20Up%20App' target='_top'>Xing Brew</a>"))
                                 ),  # end of fluidRow
                                 fluidRow(
-                                  column(6, panel_div("info", "App Status", "Include text with status, version and updates")),
-                                  column(6, panel_div("danger", "App Design and Support", "Copyright 2017 <a href='http://databrew.cc'>DataBrew Consulting Services</a>")),
+                                  column(6, panel_div("info", "App Status", "Up and running with the most recent data!")),
+                                  column(6, panel_div("danger", "App Design and Support", "Copyright 2017, <a href='http://databrew.cc'>DataBrew Consulting Services</a>")),
                                   
                                   #### FAVICON TAGS SECTION ####
                                   tags$head(tags$link(rel="shortcut icon", href="favicon.ico"))#,
@@ -209,9 +213,24 @@ ui <- dashboardPage(skin = 'blue',
 
                                 downloadButton('downloadSurvey', 'Download'),
                                 br()),
-                        tabItem(tabName = "about",
-                                h2("About"),
-                                h4('A collaboration with www.databrew.cc'))
+                        tabItem(
+                          tabName = 'about',
+                          fluidPage(
+                            fluidRow(
+                              div(img(src='logo_clear.png', align = "center"), style="text-align: center;"),
+                              h4('Built in partnership with ',
+                                 a(href = 'http://databrew.cc',
+                                   target='_blank', 'Databrew'),
+                                 align = 'center'),
+                              p('Empowering research and analysis through collaborative data science.', align = 'center'),
+                              div(a(actionButton(inputId = "email", label = "info@databrew.cc", 
+                                                 icon = icon("envelope", lib = "font-awesome")),
+                                    href="mailto:info@databrew.cc",
+                                    align = 'center')), 
+                              style = 'text-align:center;'
+                            )
+                          )
+                        )
                         
                       )))
                     
@@ -988,6 +1007,8 @@ server <- function(input, output) {
   output$geography_filter <- renderUI({
     if(input$geography){
       choices <-  sort(unique(census$Geography))
+      #remove ontario
+      choices <- choices[!grepl('Ontario', choices)]
       choices <- c('All', choices)
       choices <- choices[!grepl('Total', choices)]
       selectInput('geography_filter',
